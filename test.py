afae30ac3837c5a6ffc17221d7e9b8077d8b716b
Mridul Agarwal <mridul310703@gmail.com>
2024-04-14 00:29:17 +0530

    Round2 Data Added

--- test.py
+++ test.py
@@ -1,6 +1,35 @@
 import pandas as pd
-import talib
+import plotly.graph_objs as go
 
-df = pd.read_csv('round-1-island-data-bottle/prices_round_1_day_-1.csv', sep=';')
+df = pd.read_csv('round-2-island-data-bottle/prices_round_2_day_1.csv', sep=';')
 
-df.drop(['day', 'bid_price_2', 'bid_volume_2', 'bid_price_3', 'bid_volume_3', 'ask_price_2', 'ask_volume_2', 'ask_price_3', 'ask_volume_3'], axis=1, inplace=True)
+trace1 = go.Scatter(x=df['timestamp'], y=df['ORCHIDS'], mode='lines', name='prices', yaxis='y1')
+trace2 = go.Scatter(x=df['timestamp'], y=df['SUNLIGHT'], mode='lines', name='sunlight', yaxis='y2')
+trace3 = go.Scatter(x=df['timestamp'], y=df['HUMIDITY'], mode='lines', name='humidity', yaxis='y3')
+
+layout = go.Layout(
+    title='Variation of different parameters',
+    yaxis=dict(
+        title='prices',
+        side='left',
+        anchor='x'
+    ),
+    yaxis2=dict(
+        title='sunlight',
+        side='right',
+        overlaying='y',
+        anchor='x'
+    ),
+    yaxis3=dict(
+        title='humidity',
+        side='right',
+        overlaying='y',
+        anchor='free',
+        position=0.95
+    )
+)
+
+fig = go.Figure(data=[trace1, trace2, trace3], layout=layout)
+
+fig.show()
+
